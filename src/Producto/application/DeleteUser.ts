799d24b3dd70b49c5dbddc0d6382de08b23aff95
OSVARYC123 <osvaldo.programacionu@gruporyc.com.mx>
2023-08-18 14:43:00 -0600

    subiendo

---
+++ src/Producto/application/DeleteUser.ts
@@ -0,0 +1,19 @@
+
+import { ProductoRepository } from "../domain/User-repository";
+
+
+export class DelUserId {
+    constructor(readonly productoRepository: ProductoRepository) { }
+
+    async Userid (Correo:string) {
+        return this.productoRepository.deleteById(Correo)
+            .then((productoResolve) => {
+                console.log(productoResolve);
+                return productoResolve;
+            }).catch((error) => {
+                console.log(error)
+            })
+    }
+}
+
+
---
+++ src/main.ts
@@ -0,0 +1,22 @@
+
+import bodyParser from "body-parser";
+import express from "express";
+const cors = require('cors')
+import { config } from "./config";
+import { routes } from "./Producto/infrastructure/RouteUser";
+import "dotenv/config";
+
+  const app = express();
+
+app.use(cors())
+app.use(bodyParser.json());
+app.use("/Users", routes);
+
+
+  const { port } = config.server;
+
+  app.listen(port, () => {
+    console.log(`[APP] - corriendo puerto ${port}`);
+  });
+
+
